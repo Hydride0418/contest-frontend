617566e4372501182e13a09e1f3a6de23e2d1e25
yangjingping <yangjingping@bytedance.com>
2022-02-12 15:43:24 +0800

    feat: ContestManage template complete

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,66 @@
 <template>
   <div>
-    <el-container style="height: 500px; border: 1px solid #eee">
-    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-      <el-menu
-      :default-active="$route.path"
-      router>
-        <el-submenu index="1">
-          <template slot="title"><i class="el-icon-message"></i>赛事管理</template>
-          <el-menu-item index="/contest_manage">赛事管理</el-menu-item>
-        </el-submenu>
-        <el-submenu index="2">
-          <template slot="title"><i class="el-icon-message"></i>报名团队管理</template>
-          <el-menu-item index="/qualification_review">参赛资格审核</el-menu-item>
-          <el-menu-item index="/selection_review">选题信息审核</el-menu-item>
-          <el-menu-item index="/team_manage">报名团队管理</el-menu-item>
-          <el-menu-item index="/work_manage">作品管理</el-menu-item>
-        </el-submenu>
-        <el-submenu index="3">
-          <template slot="title"><i class="el-icon-message"></i>基础数据管理</template>
-          <el-menu-item index="3-1">赛区管理员管理</el-menu-item>
-          <el-menu-item index="3-2">专家库管理</el-menu-item>
-          <el-menu-item index="3-3">研究生培养单位联系人管理</el-menu-item>
-        </el-submenu>
-        <el-submenu index="4">
-          <template slot="title"><i class="el-icon-message"></i>初赛管理</template>
-          <el-menu-item index="/about">评审管理</el-menu-item>
-          <el-menu-item index="4-2">评阅指标管理</el-menu-item>
-          <el-menu-item index="4-3">评审进度管理</el-menu-item>
-          <el-menu-item index="4-4">评审结果管理</el-menu-item>
-        </el-submenu>
-        <el-submenu index="5">
-          <template slot="title"><i class="el-icon-message"></i>奖项设置</template>
-          <el-menu-item index="5-1">奖项管理</el-menu-item>
-          <el-menu-item index="5-2">参赛单位奖项设置</el-menu-item>
-          <el-menu-item index="5-3">个人奖项设置</el-menu-item>
-          <el-menu-item index="5-4">团队奖项设置</el-menu-item>
-        </el-submenu>
-        <el-submenu index="6">
-          <template slot="title"><i class="el-icon-message"></i>用户中心</template>
-          <el-menu-item index="6-1">个人密码修改</el-menu-item>
-          <el-menu-item index="6-2">修改个人信息</el-menu-item>
-        </el-submenu>
-      </el-menu>
-    </el-aside>
-    
-    <el-container>
-      <el-header style="text-align: right; font-size: 12px">
-        <el-dropdown>
-          <i class="el-icon-setting" style="margin-right: 15px"></i>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>查看</el-dropdown-item>
-            <el-dropdown-item>新增</el-dropdown-item>
-            <el-dropdown-item>删除</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
-        <span>王小虎</span>
-      </el-header>
-      
-      <el-main>
-        <router-view></router-view>
-      </el-main>
+    <el-container style="height: 800px; border: 1px solid #eee">
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <el-menu
+        :default-active="$route.path"
+        router>
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-message"></i>赛事管理</template>
+            <el-menu-item index="/contest_manage">赛事管理</el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title"><i class="el-icon-message"></i>报名团队管理</template>
+            <el-menu-item index="/qualification_review">参赛资格审核</el-menu-item>
+            <el-menu-item index="/selection_review">选题信息审核</el-menu-item>
+            <el-menu-item index="/team_manage">报名团队管理</el-menu-item>
+            <el-menu-item index="/work_manage">作品管理</el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title"><i class="el-icon-message"></i>基础数据管理</template>
+            <el-menu-item index="3-1">赛区管理员管理</el-menu-item>
+            <el-menu-item index="3-2">专家库管理</el-menu-item>
+            <el-menu-item index="3-3">研究生培养单位联系人管理</el-menu-item>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title"><i class="el-icon-message"></i>初赛管理</template>
+            <el-menu-item index="/about">评审管理</el-menu-item>
+            <el-menu-item index="4-2">评阅指标管理</el-menu-item>
+            <el-menu-item index="4-3">评审进度管理</el-menu-item>
+            <el-menu-item index="4-4">评审结果管理</el-menu-item>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title"><i class="el-icon-message"></i>奖项设置</template>
+            <el-menu-item index="5-1">奖项管理</el-menu-item>
+            <el-menu-item index="5-2">参赛单位奖项设置</el-menu-item>
+            <el-menu-item index="5-3">个人奖项设置</el-menu-item>
+            <el-menu-item index="5-4">团队奖项设置</el-menu-item>
+          </el-submenu>
+          <el-submenu index="6">
+            <template slot="title"><i class="el-icon-message"></i>用户中心</template>
+            <el-menu-item index="6-1">个人密码修改</el-menu-item>
+            <el-menu-item index="6-2">修改个人信息</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>  
+      <el-container>
+        <el-header style="text-align: right; font-size: 12px">
+          <el-dropdown>
+            <i class="el-icon-setting" style="margin-right: 15px"></i>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>查看</el-dropdown-item>
+              <el-dropdown-item>新增</el-dropdown-item>
+              <el-dropdown-item>删除</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <span>admin</span>
+        </el-header>
+        
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
     </el-container>
-  </el-container>
   </div>
 </template>
 
--- src/views/contest_manage/ContestManage.vue
+++ src/views/contest_manage/ContestManage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main_div">
-<el-row type="flex" justify="space-between">
+    <el-row type="flex" justify="space-between">
       <el-form label-width="80px">
       <el-form-item label="赛事搜索" type="flex">
         <el-row type="flex" justify="space-between">
@@ -18,9 +18,27 @@
       <el-button icon="el-icon-upload2" class="new_contest">导出赛事信息</el-button>
     </div>
     </el-row>
+    <el-table :data="contestData" style="width: 100%">
+      <el-table-column label="操作" width="400px">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text">修改赛事信息</el-button>
+          <el-button type="text">奖项管理</el-button>
+          <el-button type="text">分赛管理</el-button>
+          <el-button type="text">赛道管理</el-button>
+        </template>
+      </el-table-column>
+      <el-table-column prop="contest_name" label="赛事名称"></el-table-column>
+      <el-table-column prop="time_limit" label="时间限制数"></el-table-column>
+      <el-table-column prop="ques_number" label="赛题数"></el-table-column>
+      <el-table-column prop="archive_status" label="归档状态"></el-table-column>
+    </el-table>
   </div>
 </template>
 <style>
+  .main_div {
+    display: flex;
+    flex-direction: column;
+  }
   .el-col {
     border-radius: 4px;
   }
@@ -57,3 +75,33 @@
     flex-direction: column;
   }
 </style>
+
+<script>
+  export default {
+      data() {
+        return {
+          contestData: [{
+            contest_name:"大赛1",
+            time_limit:3,
+            ques_number:2,
+            archive_status: "已归档"
+          },{
+            contest_name:"大赛2",
+            time_limit:3,
+            ques_number:2,
+            archive_status: "已归档"
+          },{
+            contest_name:"大赛3",
+            time_limit:3,
+            ques_number:2,
+            archive_status: "未归档"
+          },{
+            contest_name:"大赛4",
+            time_limit:3,
+            ques_number:2,
+            archive_status: "已归档"
+          }]
+        }
+      }
+    }
+</script>
